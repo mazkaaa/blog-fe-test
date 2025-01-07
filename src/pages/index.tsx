4781d7f0aa969aec6a568fff669fe575df9c99c4
Muhammad Azka <hello.mazka@gmail.com>
2025-01-07 20:36:22 +0700

    ci: redeploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { useAuth } from '@/components/contexts';
 
 export default function Home() {
   const { status, setupLogin, authData } = useAuth();
-
   return (
     <div>
       <PostSection />
